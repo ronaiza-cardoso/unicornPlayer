79251ff7f239df5f2b32d7187e28bc62cbfb271f
Ronaiza Cardoso <ronaizacard@gmail.com>
2020-07-15 16:25:30 +0100

    feat(Home): add initial style

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,21 +1,53 @@
 import React from 'react';
-import { SafeAreaView, View, Text } from 'react-native';
+import { SafeAreaView, View, Text, StyleSheet } from 'react-native';
 
 import { useAuth } from '../../navigation/AuthProvider';
+import { width, height } from '../../utils/dimensions';
 
 import Button from '../../components/Button';
 
 function Home() {
-  const { logOut } = useAuth();
+  const { user, logOut } = useAuth();
+
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Hello from Home</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.welcomeContainer}>
+        <Text style={styles.welcomeText}>
+          Welcome {user.name} to 🦄 paradise!
+        </Text>
       </View>
 
-      <Button title="Log out" onPress={logOut} />
+      <View style={styles.footer}>
+        <Button title="Log out" onPress={logOut} />
+      </View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FEFAFA',
+    position: 'relative',
+  },
+  welcomeContainer: {},
+  welcomeText: {
+    marginTop: 20,
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'rebeccapurple',
+  },
+  footer: {
+    position: 'absolute',
+    bottom: 0,
+    width,
+    height: 90,
+    backgroundColor: 'rebeccapurple',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default Home;
